ca6e91a60509f42501ad29421b08af552ea4c8b2
alex-mal79 <a-79maleev@yandex.ru>
2025-08-28 14:25:48 +0300

    add barench mac-to-ftp

--- src/js/functions/macToAccessList.js
+++ src/js/functions/macToAccessList.js
@@ -3,5 +3,12 @@
 // MAC-адресов access-list
 //================================================================
 export const macToAccessList = () => {
-
+    const getMacBtn = document.querySelector('[data-get-mac-btn]');
+    if(getMacBtn){
+        const inputGetMac = document.querySelector('[data-get-mac]');
+        console.log('test');
+        getMacBtn.addEventListener("click", () => {
+            console.log(inputGetMac.value);
+        });
+    }
 }
