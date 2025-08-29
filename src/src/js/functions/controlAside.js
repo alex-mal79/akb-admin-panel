1fa555cae44a477e803b505e1e5aa942d8560577
alex-mal79 <a-79maleev@yandex.ru>
2025-08-29 19:19:51 +0300

    new project design

---
+++ src/src/js/functions/controlAside.js
@@ -0,0 +1,19 @@
+//================================================================
+// Кнопки управления боковой панелю
+//================================================================
+export const controlAside = () => {
+    const btnResizeNav = document.querySelector('[data-btn-resize-nav]');
+    const pageWrapper  = document.querySelector('[data-page-wrapper]');
+    if(btnResizeNav && pageWrapper){
+        btnResizeNav.addEventListener("click", function(e){
+            let currentButton = e.currentTarget;
+            let currentButtonParent = currentButton.closest('[data-aside]');
+            if(currentButtonParent){
+                currentButtonParent.classList.toggle('minimize');
+                pageWrapper.classList.toggle('extensive');
+                setTimeout(() => { currentButtonParent.classList.toggle('hover'); }, 100);
+            }
+        });
+
+    }
+}
