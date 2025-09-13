426a8c1cf38465557d29c8dae01ca842b30e4937
alex-mal79 <a-79maleev@yandex.ru>
2025-09-13 23:58:00 +0300

    start project AKB Admin Panel

---
+++ src/js/functions/headerScroll.js
@@ -0,0 +1,24 @@
+//================================================================
+// Шапка при скролле
+//================================================================
+export const headerScroll = () => {
+    const header = document.querySelector('[data-scroll]');
+
+    if(header){
+        let lastScroll = 0;
+        const defaultOffset = header.dataset.scroll;
+        const scrollPosition = () => window.scrollY || document.documentElement.scrollTop;
+        const containHide = () => header.classList.contains('scroll--active');
+        window.addEventListener('scroll', () => {
+            if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+                //scroll down
+                header.classList.add('scroll--active');
+            }
+            else if(scrollPosition() < lastScroll && containHide()){
+                //scroll up
+                header.classList.remove('scroll--active');
+            }
+            lastScroll = scrollPosition();
+        });
+    }
+}
