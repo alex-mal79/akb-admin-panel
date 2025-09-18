886d5f2dbd56cc01bc6691779bcfc0afe3322b1b
alex-mal79 <a-79maleev@yandex.ru>
2025-09-18 17:59:22 +0300

    создание скрипта поведения пунктов 1-го уровня бокового уровня

--- src/js/functions/dropDown.js
+++ src/js/functions/dropDown.js
@@ -2,66 +2,100 @@
 //Меню DropDown
 //================================================================
 export const dropDown = () => {
-//     const dropMenuButtons = document.querySelectorAll('[data-drop-btn]');
-//     const mediaQuery = window.matchMedia('(min-width: 768px)');
-//     if(dropMenuButtons.length){
-//         for(let i = 0; i < dropMenuButtons.length; i++){
-//             let dropMenuButton = dropMenuButtons[i];
-//             dropMenuButton.addEventListener("click", function(e){
-//                 let currentButton = e.currentTarget;
-//                 let dropDownParent = currentButton.closest('[data-drop-parent]');
+    const dropBtns = Array.from(document.querySelectorAll('[data-drop-btn]'));
 
-//                 function ScreenChange(e) {
-//                     if (e.matches) {
-//                         for(let i = 0; i < dropMenuButtons.length; i++){
-//                             let dropMenuButton = dropMenuButtons[i];
-//                             if(dropMenuButton !== currentButton){
-//                                 dropMenuButton.closest('[data-drop-parent]').classList.remove('drop--active');
-//                             }
-//                         }
-//                     }
-//                 }
-//                 mediaQuery.addListener(ScreenChange);
-//                 ScreenChange(mediaQuery);
+    if(dropBtns.length > 0){
 
-//                 dropDownParent.classList.toggle('drop--active');
-//             });
-//         }
-//     }
+        dropBtns.forEach((dropBtn) => {
+            dropBtn.addEventListener("click", function(e){
+                e.preventDefault();
+                let countParent = countDropParent(dropBtn);
 
-    const navBtns = Array.from(document.querySelectorAll('[data-drop-btn]'));
+                if(countParent == 1){
+                    let i = 0;
+                    while(i < dropBtns.length){
+                        console.log('i = ', i, 'dropBtns.length = ', dropBtns.length);
 
-    navBtns.forEach((navBtn) => {
-        navBtn.addEventListener("click", function(e){
-            e.preventDefault();
+                        let dropBtn = dropBtns[i];
+                        if(dropBtn.parentNode.closest('.drop--active')){
+                            dropBtn.parentNode.classList.remove('drop--active');
+                        }
+                        if(dropBtn.nextElementSibling !== null){
+                                dropBtn.nextElementSibling.classList.remove('drop--active');
+                        }
+                        i++;
+                    }
+                    dropBtn.parentNode.classList.add('drop--active');
+                    if(dropBtn.nextElementSibling !== null){
+                        dropBtn.nextElementSibling.classList.add('drop--active');
+                    }
+                }
+                // else{
 
-            let dropParent = e.target.closest('[data-drop-parent]');
-            let dropBtn = e.target.closest('[data-drop-btn]');
-            let dropBlock = e.currentTarget.nextElementSibling;
-            let dropParentChildren = dropParent.childNodes;
+                // }
 
-            let dropParents = document.querySelectorAll('[data-drop-parent]');
+                // if(dropBtn.parentNode.closest(".drop--active")){
+                //     console.log("parents = ", parents.length);
 
-            console.log("dropParentBtn = ", dropParent);
-            console.log("dropBtn = ", dropBtn);
-            console.log("dropBlock = ", dropBlock);
-            console.log("dropParentChildren = ", dropParentChildren);
+                //     console.log("dropParent = ", dropParent);
+                //     console.log("dropBtnParent = ", dropBtn.parentNode);
+                // }
+                // else{
+                //     console.log("parents = ", parents.length);
 
-            console.log("allParent = ", dropParents);
+                //     console.log("dropParent = ", dropParent);
+                //     console.log("нет стиля drop--active");
+                // }
+
+                // for(let i = 0; i < dropBtns.length; i++){
+                //     let dropBtn = dropBtns[i];
+                //     if(dropBtnItem.parentNode.closest(".drop--active")){
+                //         console.log("dropBtnItemParentNode = ", dropBtn.parentNode);
+                //     }
+                // }
+
+                // for(let i = 0; i <= dropParents.length; i++){
+                //     console.log("i = ", i);
+                // }
+
+                // let dropParent = e.target.closest('[data-drop-parent]');
+                // let dropBtn = e.target.closest('[data-drop-btn]');
+                // let dropBlock = e.currentTarget.nextElementSibling;
+                // let dropParentChildren = dropParent.childNodes;
+
+                // console.log("dropParentBtn = ", dropParent);
+                // console.log("dropBtn = ", dropBtn);
+                // console.log("dropBlock = ", dropBlock);
+                // console.log("dropParentChildren = ", dropParentChildren);
+                // console.log("allParent = ", dropParents);
+
+                // let elementParentClosest = dropParent.parentNode.closest('[data-drop-parent]');
+                // if(elementParentClosest == null){
+                //     console.log("Нет родительского элемента ");
+                // }
+                // else{
+                //     console.log("test = ", elementParentClosest);
+                // }
+            });
         });
-    });
+        // Функция возврата количество радительских элементов с атрибутом data-drop-parent
+        const countDropParent = (dropBtn) => {
+            let parents = [];
+            let countParent = 0;
 
-        // function showItemsNav(e){
+            while(dropBtn.parentNode.closest('[data-drop-parent]') && dropBtn.parentNode.nodeName.toLowerCase() != 'body') {
+                dropBtn = dropBtn.parentNode;
+                parents.push(dropBtn);
+            }
 
-        //     console.log("navBtn", navBtn);
-        //     e.preventDefault();
-        //     let currentBtn = e.target.closest('[data-drop-btn]');
-        //     let currentParent = e.target.closest('[data-drop-parent]');
-        //     let currentElementSibling = e.currentTarget.nextElementSibling;
-        //     console.log("currentBtn", currentBtn);
-        //     console.log("currentParent", currentParent);
-        //     console.log("currentElementSibling", currentElementSibling);
-        // }
+            parents.forEach((parent) => {
+                if(parent.hasAttribute('data-drop-parent')){
+                    countParent++;
+                }
+            });
 
+            return countParent;
+        }
 
+    }
 }
