da9ec579e9bd04031eddfd72819e5b00f2b8fb26
alex-mal79 <a-79maleev@yandex.ru>
2025-09-19 13:45:46 +0300

    создан скрипт управления меню боковой панели

--- src/js/functions/dropDown.js
+++ src/js/functions/dropDown.js
@@ -2,69 +2,43 @@
 //Меню DropDown
 //================================================================
 export const dropDown = () => {
+
+    const navBtns = Array.from(document.querySelectorAll('[data-nav-btn]'));
     const dropBtns = Array.from(document.querySelectorAll('[data-drop-btn]'));
 
     if(dropBtns.length > 0){
-
         dropBtns.forEach((dropBtn) => {
             dropBtn.addEventListener("click", function(e){
                 e.preventDefault();
-                let countParent = countDropParent(dropBtn);
-
-                // if(countParent == 1){
-                    let i = 0;
-                    while(i < dropBtns.length){
-                        let dropBtn = dropBtns[i];
-                        if(dropBtn.parentNode.closest('.drop--active')){
-                            dropBtn.parentNode.classList.remove('drop--active');
-                        }
-                        if(dropBtn.nextElementSibling !== null){
-                                dropBtn.nextElementSibling.classList.remove('drop--active');
-                        }
-                        i++;
-                    }
-                    dropBtn.parentNode.classList.add('drop--active');
-                    if(dropBtn.nextElementSibling !== null){
-                        dropBtn.nextElementSibling.classList.add('drop--active');
-                    }
-                // }
-                // else{
-
-                // }
-
-                // let dropParent = e.target.closest('[data-drop-parent]');
-                // let dropBtn = e.target.closest('[data-drop-btn]');
-                // let dropBlock = e.currentTarget.nextElementSibling;
-                // let dropParentChildren = dropParent.childNodes;
-
-                // console.log("dropParentBtn = ", dropParent);
-                // console.log("dropBtn = ", dropBtn);
-                // console.log("dropBlock = ", dropBlock);
-                // console.log("dropParentChildren = ", dropParentChildren);
-                // console.log("allParent = ", dropParents);
+                showItem(dropBtns, dropBtn);
             });
         });
-        // Функция возврата количество радительских элементов с атрибутом data-drop-parent
-        const countDropParent = (dropBtn) => {
-            let parents = [];
-            let countParent = 0;
+    }
 
-            while(dropBtn.parentNode.closest('[data-drop-parent]') && dropBtn.parentNode.nodeName.toLowerCase() != 'body') {
-                dropBtn = dropBtn.parentNode;
-                parents.push(dropBtn);
-            }
-
-            console.log(parents);
-
-
-            parents.forEach((parent) => {
-                if(parent.hasAttribute('data-drop-parent')){
-                    countParent++;
-                }
+    if(navBtns.length > 0){
+        navBtns.forEach((navBtn) => {
+            navBtn.addEventListener("click", function(e){
+                e.preventDefault();
+                showItem(navBtns, navBtn);
             });
+        });
+    }
 
-            return countParent;
+    const showItem = (clickBtns, clickBtn) => {
+        let i = 0;
+        while(i < clickBtns.length){
+            let clickBtn = clickBtns[i];
+            if(clickBtn.parentNode.closest('.drop--active')){
+                clickBtn.parentNode.classList.remove('drop--active');
+            }
+            if(clickBtn.nextElementSibling !== null){
+                    clickBtn.nextElementSibling.classList.remove('drop--active');
+            }
+            i++;
         }
-
+        clickBtn.parentNode.classList.add('drop--active');
+        if(clickBtn.nextElementSibling !== null){
+            clickBtn.nextElementSibling.classList.add('drop--active');
+        }
     }
 }
